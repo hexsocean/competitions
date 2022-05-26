a715ae581fd53c0cdfec1d129ad07be77fb8f96f
Hex <lumengeared@gmail.com>
2022-05-26 10:02:22 +0800

    Added template

---
+++ cpp-io-template/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    freopen("{[.NAME.]}in.txt", "r", stdin);
+    freopen("{[.NAME.]}out.txt", "w", stdout);
+    int take = 0;
+    cin >> take;
+    for (int i = 0; i < take; i++)
+    {
+        char temp;
+        cin >> temp;
+        cout << temp;
+    }
+}
